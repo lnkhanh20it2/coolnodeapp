7fce5406f59fb9d250aadce4122068badeb51614
Khanh <lnkhanh.20it2@vku.udn.vn>
2023-10-09 00:58:51 +0700

    continue handlebar

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
-const http = require('http');
-const port = process.env.PORT || 3000
-const server = http.createServer((req, res) => {
-res.statusCode = 200;
-res.setHeader('Content-Type', 'text/html');
-res.end('<h1>Hello World My name is Le Ngoc Khanh</h1>');
+const app = require("express")();
+var path = require("path");
+//view engine setup
+app.set("views", path.join(__dirname, "views")); //setting views directory for views.
+app.set("view engine", "hbs"); //setting view engine as handlebars
+app.get("/", (req, res) => {
+let peopleList = getRandomList();
+res.render("index", { people: peopleList }); //passing list of people to our index.hbs file.
 });
-server.listen(port,() => {
-console.log(`Server running at port `+port);
-});
+let getRandomList = () => {
+let list = ["ada", "turing", "lovelace", "neumann", "gracehopper"];
+let limit = Math.floor(Math.random() * (list.length - 1 - 0) + 0); 
+//generating random number between 0 & 4
+return list.slice(limit);
+};
+app.listen(5000);
